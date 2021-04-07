c872d7943d581ac7d16072aeb70878994b30650d
Thuan Saraiva <thuan.saraiva@pathfind.com.br>
2021-04-07 19:34:21 -0300

    first commit code

---
+++ src/main/java/com/itegra/auditcom/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.itegra.auditcom.repository;
+
+import com.itegra.auditcom.domain.Authority;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+
+/**
+ * Spring Data R2DBC repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends R2dbcRepository<Authority, String> {}
